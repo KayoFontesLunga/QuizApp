cc5d9bdddd5d9d03673921e4a13897ae769dee14
KayoFontesLunga <kayolunga84@gmail.com>
2025-04-30 14:03:38 -0300

    :recycle: refactor: Add DataAnnotations

--- DTOs/UserRegistrationDTO.cs
+++ DTOs/UserRegistrationDTO.cs
@@ -1,8 +1,14 @@
-﻿namespace QuizApp.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuizApp.DTOs;
 
 public class UserRegistrationDTO
 {
+    [Required(ErrorMessage = "Name is required")]
     public string Name { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email address")]
     public string Email { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Password is required")]
     public string Password { get; set; } = string.Empty;
 }
