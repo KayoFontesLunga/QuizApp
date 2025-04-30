7c2f3686187626fe5dc9f30c2c73438412250fb6
KayoFontesLunga <kayolunga84@gmail.com>
2025-04-30 13:17:38 -0300

    :sparkles: feat: Create DTO for user registration

---
+++ DTOs/UserRegistrationDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace QuizApp.DTOs;
+
+public class UserRegistrationDTO
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
